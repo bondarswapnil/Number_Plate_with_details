c8a3a728c3afcca263ddf2c8886ff09cae241f2d
Swapnil Bondar <114136262+bondarswapnil@users.noreply.github.com>
2023-02-19 00:02:14 +0530

    Add files via upload

---
+++ slicing_number.py
@@ -0,0 +1,7 @@
+#Program to print Number Plate with details
+
+n_Plate = input("Enter your Vehicle Number : ")
+print("State is  :" ,n_Plate[0:2])
+print("State Code  :" ,n_Plate[2:4])
+print("Serial code  :" ,n_Plate[4:6])
+print("Registration is  :" ,n_Plate[6:])
